35f1740daafa3ebaf098fc1a7cb1cc00225a9ba7
BipashaKushwah <bipasha9551@gmail.com>
2025-09-25 10:13:14 +0530

    Time: 41 ms (32.53%), Space: 72.5 MB (49.86%) - LeetHub

---
+++ 0015-3sum/0015-3sum.js
@@ -0,0 +1,35 @@
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var threeSum = function(nums) {
+    nums.sort((a, b) => a - b); // Step 1: sort
+    let result = [];
+
+    for (let i = 0; i < nums.length - 2; i++) {
+        if (i > 0 && nums[i] === nums[i - 1]) continue; // skip duplicate i
+
+        let l = i + 1, r = nums.length - 1;
+
+        while (l < r) {
+            let sum = nums[i] + nums[l] + nums[r];
+
+            if (sum === 0) {
+                result.push([nums[i], nums[l], nums[r]]);
+                l++;
+                r--;
+
+                // skip duplicates
+                while (l < r && nums[l] === nums[l - 1]) l++;
+                while (l < r && nums[r] === nums[r + 1]) r--;
+            } 
+            else if (sum < 0) {
+                l++; // need bigger number
+            } 
+            else {
+                r--; // need smaller number
+            }
+        }
+    }
+    return result;
+};
