11e8dc2c4615645639eae629699879f2fff0f7c7
BipashaKushwah <bipasha9551@gmail.com>
2025-10-10 12:46:53 +0530

    Time: 156 ms (44.31%), Space: 71.4 MB (50.2%) - LeetHub

---
+++ 0164-maximum-gap/0164-maximum-gap.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maximumGap = function(nums) {
+nums.sort((a, b) => a - b);
+let maxGap = 0;
+for (let i = 1; i < nums.length; i++) {
+  maxGap = Math.max(maxGap, nums[i] - nums[i - 1]);
+}
+return maxGap;
+
+};
