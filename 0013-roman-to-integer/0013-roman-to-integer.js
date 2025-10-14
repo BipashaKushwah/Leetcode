ca9dc0ef54462d9c1cf301d87d09d403ae528682
BipashaKushwah <bipasha9551@gmail.com>
2025-10-14 10:40:23 +0530

    Time: 8 ms (23.66%), Space: 60.4 MB (79.12%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.js
@@ -0,0 +1,34 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var romanToInt = function(s) {
+   
+  const romanMap = {
+    'I': 1,
+    'V': 5,
+    'X': 10,
+    'L': 50,
+    'C': 100,
+    'D': 500,
+    'M': 1000
+  };
+
+  let total = 0;
+
+  for (let i = 0; i < s.length; i++) {
+    let curr = s[i];
+    let next = s[i + 1];
+
+    if (romanMap[next] > romanMap[curr]) {
+      total -=romanMap[curr];
+
+    } else {
+      total += romanMap[curr];
+    }
+  }
+
+  return total;
+};
+
+console.log(romanToInt("III"));
