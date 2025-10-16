1f22e5892e07924dea4e77c389bfad254f13e10a
BipashaKushwah <bipasha9551@gmail.com>
2025-10-16 10:14:14 +0530

    Time: 0 ms (100%), Space: 56.2 MB (95.79%) - LeetHub

---
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function(nums) {
+    if (nums.length === 0) return 0;
+
+    let k = 1; 
+
+    for (let i = 1; i < nums.length; i++) {
+        if (nums[i] !== nums[i - 1]) {
+            nums[k] = nums[i];
+            k++;
+        }
+    }
+
+    return k;
+};
+
+
+let nums = [1, 1, 2];
+let k = removeDuplicates(nums);
+
+console.log(k);        
+console.log(nums);     
