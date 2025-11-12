ec6db0d5dda862868b53c59cdb918baff54daca6
BipashaKushwah <bipasha9551@gmail.com>
2025-11-12 10:33:22 +0530

    Time: 7 ms (75.28%), Space: 63.2 MB (24.94%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.js
+++ 0645-set-mismatch/0645-set-mismatch.js
@@ -2,22 +2,22 @@
  * @param {number[]} nums
  * @return {number[]}
  */
-var findErrorNums = function(nums) {
-     const seen = new Set();
-  let duplicate = -1;
-  let n = nums.length;
-  let sum = 0;
+var findErrorNums = function (nums) {
+    const seen = new Set();
+    let duplicate = -1;
+    let n = nums.length;
+    let sum = 0;
 
-  for (let num of nums) {
-    if (seen.has(num)) {
-      duplicate = num;   // found the repeated number
+    for (let num of nums) {
+        if (seen.has(num)) {
+            duplicate = num;   // found the repeated number
+        }
+        seen.add(num);
+        sum += num;
     }
-    seen.add(num);
-    sum += num;
-  }
 
-  const expectedSum = (n * (n + 1)) / 2;
-  const missing = expectedSum - (sum - duplicate);
+    const expectedSum = (n * (n + 1)) / 2;
+    const missing = expectedSum - (sum - duplicate);
 
-  return [duplicate, missing];
+    return [duplicate, missing];
 };
