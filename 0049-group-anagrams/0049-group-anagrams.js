1d2bb89dcaef00086fcfa533eaed7ed055d7722b
BipashaKushwah <bipasha9551@gmail.com>
2025-11-13 10:09:36 +0530

    Time: 40 ms (32.36%), Space: 70.8 MB (7.22%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.js
@@ -0,0 +1,15 @@
+/**
+ * @param {string[]} strs
+ * @return {string[][]}
+ */
+var groupAnagrams = function(strs) {
+    const groups = {};
+    for (const str of strs) {
+        const sorted = str.split('').sort().join('');
+        if (!groups[sorted]) {
+            groups[sorted] = [];
+        }
+        groups[sorted].push(str);
+    }
+    return Object.values(groups);
+};
