3722858da83ccf8d68a6713924dd0c5a61d4bac7
BipashaKushwah <bipasha9551@gmail.com>
2025-11-15 09:56:45 +0530

    Time: 8 ms (23.91%), Space: 56.2 MB (50.26%) - LeetHub

---
+++ 0389-find-the-difference/0389-find-the-difference.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {character}
+ */
+var findTheDifference = function(s, t) {
+
+    const map = new Map();
+    
+    // Count characters in s
+    for (let char of s) {
+        map.set(char, (map.get(char) || 0) + 1);
+    }
+    
+    // Check characters in t
+    for (let char of t) {
+        if (!map.has(char) || map.get(char) === 0) {
+            return char;
+        }
+        map.set(char, map.get(char) - 1);
+    }  
+};
