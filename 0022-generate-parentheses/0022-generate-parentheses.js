3b059be819a6320814e63f0395d679f2441ff9cd
BipashaKushwah <bipasha9551@gmail.com>
2025-12-12 11:09:46 +0530

    Time: 1 ms (46.31%), Space: 56.7 MB (21.62%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number} n
+ * @return {string[]}
+ */
+var generateParenthesis = function(n) {
+    const result = [];
+    
+    function backtrack(s, open, close) {
+        if (s.length === 2 * n) {
+            result.push(s);
+            return;
+        }
+        if (open < n) {
+            backtrack(s + '(', open + 1, close);
+        }
+        if (close < open) {
+            backtrack(s + ')', open, close + 1);
+        }
+    }
+    
+    backtrack('', 0, 0);
+    return result;
+};
