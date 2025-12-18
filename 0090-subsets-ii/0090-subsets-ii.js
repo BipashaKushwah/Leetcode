47427f68300146d2f3d2f6e94130fa3c7a95031f
BipashaKushwah <bipasha9551@gmail.com>
2025-12-18 10:55:53 +0530

    Time: 0 ms (100%), Space: 59.6 MB (61.35%) - LeetHub

---
+++ 0090-subsets-ii/0090-subsets-ii.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var subsetsWithDup = function(nums) {
+    nums.sort((a, b) => a - b); // Step 1: sort
+    let result = [];
+
+    function backtrack(start, path) {
+        result.push([...path]);
+
+        for (let i = start; i < nums.length; i++) {
+            // Skip duplicates at same level
+            if (i > start && nums[i] === nums[i - 1]) continue;
+
+            path.push(nums[i]);
+            backtrack(i + 1, path);
+            path.pop();
+        }
+    }
+
+    backtrack(0, []);
+    return result;
+};
