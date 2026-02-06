5fe86d546169d86cd8bd62282e86dc5de6116c02
BipashaKushwah <bipasha9551@gmail.com>
2026-02-06 10:07:00 +0530

    Time: 0 ms (100%), Space: 65.4 MB (83.09%) - LeetHub

--- 0134-gas-station/0134-gas-station.js
+++ 0134-gas-station/0134-gas-station.js
@@ -3,20 +3,20 @@
  * @param {number[]} cost
  * @return {number}
  */
-var canCompleteCircuit = function(gas, cost) {
+var canCompleteCircuit = function (gas, cost) {
     let total_tank = 0;
     let current_tank = 0;
     let start = 0;
-    
+
     for (let i = 0; i < gas.length; i++) {
         total_tank += gas[i] - cost[i];
         current_tank += gas[i] - cost[i];
-        
+
         if (current_tank < 0) {
             current_tank = 0;
             start = i + 1;
         }
     }
-    
+
     return total_tank >= 0 ? start : -1;
 };
