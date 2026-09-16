1587274766f2114fb57428031d04875333c5da38
BipashaKushwah <bipasha9551@gmail.com>
2026-09-16 22:27:15 +0530

    Time: 0 ms (100%), Space: 53.8 MB (38.53%) - LeetHub

--- 0027-remove-element/0027-remove-element.js
+++ 0027-remove-element/0027-remove-element.js
@@ -4,32 +4,14 @@
  * @return {number}
  */
 var removeElement = function(nums, val) {
-     let k = 0; 
 
-    for (let i = 0; i < nums.length; i++) {
-        if (nums[i] !== val) {
-            nums[k] = nums[i];
-            k++;
+     let j=0;
+     for(let i=0;i<nums.length;i++){
+        if(nums[i]!==val){
+            nums[j]=nums[i];
+            j++;
         }
-    }
+     }
+     return j;
 
-    return k;
-};
-var removeElement = function(nums, val) {
-     let k = 0; 
-
-    for (let i = 0; i < nums.length; i++) {
-        if (nums[i] !== val) {
-            nums[k] = nums[i];
-            k++;
-        }
-    }
-
-    return k;
-};
-let nums = [3, 2, 2, 3];
-let val = 3;
-let k = removeElement(nums, val);        
- 
- console.log(k);        
-console.log(nums);  
+}
