50ecaf17a68f0736cf358d32aa2aea78d3753700
BipashaKushwah <bipasha9551@gmail.com>
2026-09-16 22:10:04 +0530

    Time: 29 ms (40.16%), Space: 54.3 MB (77.74%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -1,19 +1,17 @@
 
 const twoSum = (nums, target) => {
-  const map = {};
 
-  for (let i = 0; i < nums.length; i++) {
-    const complement = target - nums[i];
-
-    for (let key in map) {
-      if (+key === complement) {
-        return [map[key], i];
-      }
+    for(let i=0;i<nums.length;i++){
+        for(let j=i+1;j<nums.length;j++){
+            if(nums[i]+nums[j]===target){
+                 return [i, j];
+            }
+        }
     }
 
-    map[nums[i]] = i;
-  }
 
-  return [];
-};
+  };
+
+
+
 
